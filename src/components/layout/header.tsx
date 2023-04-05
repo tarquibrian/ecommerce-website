2ab9fc2f6f6a58753db366de71b82f4e0b8075c5
brian tarqui <tarquibrian@gmail.com>
2023-04-04 23:01:31 -0400

    test

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,38 @@
+import { link } from "fs";
 import React from "react";
 
+interface HeaderDatas {
+  id: number;
+  label: string;
+  link: string;
+}
+
+const HeaderData: HeaderDatas[] = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 3,
+    label: "Home",
+    link: "/",
+  },
+];
+
 const Header = () => {
-  return <nav>Header</nav>;
+  return (
+    <nav>
+      {HeaderData.map((items) => {
+        return <h1>{items.id}</h1>;
+      })}
+    </nav>
+  );
 };
 
 export default Header;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globalStyles.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Chivo+Mono:wght@200;300;400;500;600;700;800&family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:wght@200;300;400;500;600;700;800;900&display=swap');
+        </style>
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
