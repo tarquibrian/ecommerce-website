0e42640fc092e175da8a498edf11b6911b1775c5
brian tarqui <tarquibrian@gmail.com>
2023-04-05 23:41:34 -0400

    header data created

---
+++ src/data/headerData.ts
@@ -0,0 +1,23 @@
+interface HeaderDatas {
+  id: number;
+  label: string;
+  link: string;
+}
+
+const HeaderData: HeaderDatas[] = [
+  {
+    id: 1,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    label: "Home",
+    link: "/",
+  },
+  {
+    id: 3,
+    label: "Home",
+    link: "/",
+  },
+];
