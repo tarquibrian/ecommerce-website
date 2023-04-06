f512005ed4df37e49bbf3f63cedeb67224cc58ea
brian tarqui <tarquibrian@gmail.com>
2023-04-06 14:44:38 -0400

    hero updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
 import Layout from "@/components/layout/layout";
+import Hero from "@/components/sections/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <Layout>hola</Layout>;
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
 }
