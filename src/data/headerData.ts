84624fa1c7dc6eef3fc49f12b401c9cf6aa9776f
brian tarqui <tarquibrian@gmail.com>
2023-04-05 23:58:18 -0400

    header data junk code removed

--- src/data/headerData.ts
+++ src/data/headerData.ts
@@ -47,28 +47,3 @@ export const HeaderData: HeaderDatas = {
     },
   ],
 };
-// {
-//   id: 1,
-//   label: "Home",
-//   link: "/",
-// },
-// {
-//   id: 2,
-//   label: "Products",
-//   link: "/products",
-// },
-// {
-//   id: 3,
-//   label: "Testimonials",
-//   link: "/testimonials",
-// },
-// {
-//   id: 4,
-//   label: "Blog",
-//   link: "/blog",
-// },
-// {
-//   id: 5,
-//   label: "Contact",
-//   link: "/contact",
-// },
