b94c01e4bfee64cf98341519a84d68cd047d16c5
brian tarqui <tarquibrian@gmail.com>
2023-04-06 22:17:49 -0400

    HERO STYELS OPTMIZD

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -6,7 +6,15 @@ const Hero = () => {
     <section id="hero">
       <Header />
       <div className="hero__container">
-        <div className="hero__container-title"></div>
+        <div className="hero__container-title">
+          <h1>
+            URBAN MODERN FASHION
+            <br />
+            FOR ALL TASTES
+            <br />
+            FOR ALL GENERATIONS
+          </h1>
+        </div>
         <div className="hero__container-desc"></div>
         <div className="hero__container-img"></div>
       </div>
