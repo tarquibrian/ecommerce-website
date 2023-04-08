0f1ee1daf8793551572db5b8405bcc5971b371ab
brian tarqui <tarquibrian@gmail.com>
2023-04-07 23:47:12 -0400

    blog

--- src/components/sections/blog.tsx
+++ src/components/sections/blog.tsx
@@ -12,17 +12,57 @@ const Blog = () => {
           <h1>Fashion News</h1>
         </header>
         <article className="blog__container-article">
-          <div className="article-card-horizontal">
+          <div className="article-card horizontal">
             <Image src={img1} alt="image from article blog" />
+            <div className="article-card-details">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quibusdam cumque eum sequi voluptatibus aut corporis pariatur
+                illum veritatis doloremque velit, cupiditate corrupti odit in
+                vel qui. Quo, accusantium possimus.
+              </p>
+              <span>View More</span>
+            </div>
           </div>
-          <div className="article-card-horizontal">
+          <div className="article-card vertical">
             <Image src={img1} alt="image from article blog" />
+            <div className="article-card-details">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quibusdam cumque eum sequi voluptatibus aut corporis pariatur
+                illum veritatis doloremque velit, cupiditate corrupti odit in
+                vel qui. Quo, accusantium possimus.
+              </p>
+              <span>View More</span>
+            </div>
           </div>
-          <div className="article-card-horizontal">
+          <div className="article-card vertical">
             <Image src={img1} alt="image from article blog" />
+            <div className="article-card-details">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quibusdam cumque eum sequi voluptatibus aut corporis pariatur
+                illum veritatis doloremque velit, cupiditate corrupti odit in
+                vel qui. Quo, accusantium possimus.
+              </p>
+              <span>View More</span>
+            </div>
           </div>
-          <div className="article-card-horizontal">
+          <div className="article-card horizontal">
             <Image src={img1} alt="image from article blog" />
+            <div className="article-card-details">
+              <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                quibusdam cumque eum sequi voluptatibus aut corporis pariatur
+                illum veritatis doloremque velit, cupiditate corrupti odit in
+                vel qui. Quo, accusantium possimus.
+              </p>
+              <span>View More</span>
+            </div>
           </div>
         </article>
       </div>
