5e7309165436500c5005ec2fa8bd7ff939ecbdb1
brian tarqui <tarquibrian@gmail.com>
2023-04-07 23:09:10 -0400

    blog component semantic

--- src/components/sections/blog.tsx
+++ src/components/sections/blog.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section id="blog">
+      <div className="blog__container">
+        <header className="blog__container-title"></header>
+        <article className="blog__container-article"></article>
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
