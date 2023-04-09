4a66accf078a38d6ce1b54fae1a19096924b7c02
brian tarqui <tarquibrian@gmail.com>
2023-04-08 23:11:03 -0400

    categories animated

--- src/components/sections/categories.tsx
+++ src/components/sections/categories.tsx
@@ -1,15 +1,53 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import img1 from "../../images/product2.jpg";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export const Categories = () => {
+  const [ref, inView] = useInView();
+  const controls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [inView, controls]);
   return (
-    <section id="categories">
+    <section id="categories" ref={ref}>
       <div className="categories__section">
-        <header className="categories__section-title">
+        <motion.header
+          className="categories__section-title"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              y: 0,
+              opacity: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: {
+              y: 100,
+              opacity: 0,
+            },
+          }}
+        >
           <h1>New Collection: ModerFashion For The Fashion-Forward</h1>
-        </header>
-        <article className="categories__section-gallery">
+        </motion.header>
+        <motion.article
+          className="categories__section-gallery"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 1, delay: 0.3 }}
+          variants={{
+            visible: {
+              y: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 0.8 },
+            },
+            hidden: { y: 100, opacity: 0 },
+          }}
+        >
           <div className="gallery-card">
             <Image src={img1} alt="image from gallery article" />
             <div className="card-button">
@@ -34,7 +72,7 @@ export const Categories = () => {
               <span>Fit Hoodie</span>
             </div>
           </div>
-        </article>
+        </motion.article>
       </div>
     </section>
   );
