2caea93f0204b5477a571d4efb17f7df4734325c
brian tarqui <tarquibrian@gmail.com>
2023-04-08 23:16:14 -0400

    blog animated

--- src/components/sections/blog.tsx
+++ src/components/sections/blog.tsx
@@ -1,19 +1,54 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import img1 from "../../images/blog1.jpg";
 import img2 from "../../images/blog2.jpg";
 import img3 from "../../images/blog3.jpg";
 import img4 from "../../images/blog4.jpg";
 import img5 from "../../images/blog5.jpg";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Blog = () => {
+  const [ref, inView] = useInView();
+  const controls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
-    <section id="blog">
+    <section id="blog" ref={ref}>
       <div className="blog__container">
-        <header className="blog__container-title">
+        <motion.header
+          className="blog__container-title"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              x: 0,
+              opacity: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: { x: 100, opacity: 0 },
+          }}
+        >
           <h1>Fashion News</h1>
-        </header>
-        <article className="blog__container-article">
+        </motion.header>
+        <motion.article
+          className="blog__container-article"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 1, delay: 0.3 }}
+          variants={{
+            visible: {
+              y: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 0.8 },
+            },
+            hidden: { y: 100, opacity: 0 },
+          }}
+        >
           <div className="article-card horizontal">
             <Image src={img2} alt="image from article blog" />
             <div className="article-card-details">
@@ -66,7 +101,7 @@ const Blog = () => {
               <span>View More</span>
             </div>
           </div>
-        </article>
+        </motion.article>
       </div>
     </section>
   );
