a465069cc44408d011ff62641ac3988ea5d32d20
brian tarqui <tarquibrian@gmail.com>
2023-04-08 21:59:04 -0400

    test

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -1,31 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 import img1 from "../../images/about1.jpg";
 import img2 from "../../images/about2.jpg";
 import Image from "next/image";
 import img3 from "../../images/about3.jpg";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+const variants = {
+  visible: { y: 0, opacity: 1, scale: 1, transition: { duration: 0.6 } },
+  hidden: { y: 200, opacity: 0, scale: 1 },
+};
 
 const About = () => {
+  const [ref, inView] = useInView();
+  const controls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
   return (
-    <section id="about">
+    <section id="about" ref={ref}>
       <div className="about__container">
         <div className="about__container-left">
-          <header className="title">
+          <motion.header
+            className="title"
+            initial="hidden"
+            animate={controls}
+            transition={{ duration: 0.8, delay: 0.3 }}
+            variants={{
+              visible: {
+                opacity: 1,
+                transition: { duration: 0.8, delay: 0.3 },
+              },
+              hidden: { opacity: 0 },
+            }}
+          >
             <h1>Coulour For Every Foot</h1>
-          </header>
-          <p>
+          </motion.header>
+          <motion.p
+            initial="hidden"
+            animate={controls}
+            transition={{ duration: 1 }}
+            variants={{
+              visible: {
+                opacity: 1,
+                transition: { duration: 1, delay: 0.3 },
+              },
+              hidden: { opacity: 0 },
+            }}
+          >
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut error
             obcaecati ex, at accusantium illo sint quo amet explicabo ipsam!
             Eaque suscipit cumque animi iste expedita voluptatum fuga similique
             quos.
-          </p>
-          <div className="images">
+          </motion.p>
+          <motion.div
+            className="images"
+            initial="hidden"
+            animate={controls}
+            transition={{ duration: 0.8 }}
+            variants={{
+              visible: {
+                y: 0,
+                opacity: 1,
+                scale: 1,
+                transition: { duration: 0.8, delay: 0.3 },
+              },
+              hidden: { y: 200, opacity: 0, scale: 1 },
+            }}
+          >
             <Image className="images-one" src={img2} alt="img2 fron about" />
             <Image className="images-two" src={img3} alt="img2 fron about" />
-          </div>
+          </motion.div>
         </div>
-        <div className="about__container-right">
+        <motion.div
+          className="about__container-right"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              x: 0,
+              opacity: 1,
+              scale: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: { x: 200, opacity: 0, scale: 1 },
+          }}
+        >
           <Image src={img1} alt="about image presentaion" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
--- src/components/sections/products.tsx
+++ src/components/sections/products.tsx
@@ -1,15 +1,50 @@
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import img1 from "../../images/product2.jpg";
+import { useInView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
 
 export const Products = () => {
+  const [ref, inView] = useInView();
+  const controls = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [inView, controls]);
   return (
-    <section id="products">
+    <section id="products" ref={ref}>
       <div className="products__container">
-        <header className="products__container-title">
+        <motion.header
+          className="products__container-title"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              x: 0,
+              opacity: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: { x: 100, opacity: 0 },
+          }}
+        >
           <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
-        </header>
-        <div className="products__container-nav">
+        </motion.header>
+        <motion.div
+          className="products__container-nav"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              x: 0,
+              opacity: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: { x: 100, opacity: 0 },
+          }}
+        >
           <ul>
             <li>T-shirt</li>
             <li>Hoddie</li>
@@ -17,8 +52,20 @@ export const Products = () => {
             <li>Shirt</li>
             <li>Cardigan</li>
           </ul>
-        </div>
-        <article className="products__container-article">
+        </motion.div>
+        <motion.article
+          className="products__container-article"
+          initial="hidden"
+          animate={controls}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          variants={{
+            visible: {
+              opacity: 1,
+              transition: { duration: 0.8, delay: 0.3 },
+            },
+            hidden: { opacity: 0 },
+          }}
+        >
           <div className="article-card">
             <Image src={img1} alt="card product from page" />
             <div className="card-details">
@@ -91,7 +138,7 @@ export const Products = () => {
               </div>
             </div>
           </div>
-        </article>
+        </motion.article>
       </div>
     </section>
   );
