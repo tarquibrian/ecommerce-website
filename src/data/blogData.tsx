402cca400f4eba1bcca0957b7049a7a0eb30dd73
brian tarqui <tarquibrian@gmail.com>
2023-04-09 20:31:02 -0400

    blog data created

---
+++ src/data/blogData.tsx
@@ -0,0 +1,52 @@
+import { StaticImageData } from "next/image";
+import img1 from "../images/blog1.jpg";
+import img2 from "../images/blog2.jpg";
+import img3 from "../images/blog3.jpg";
+import img4 from "../images/blog4.jpg";
+
+interface BlogDatas {
+  title: string;
+  articles: Articles[];
+}
+
+interface Articles {
+  id: number;
+  path: string | StaticImageData;
+  title: string;
+  desc: string;
+  button: string;
+}
+
+export const BlogData: BlogDatas = {
+  title: "Fashion News",
+  articles: [
+    {
+      id: 1,
+      path: img1,
+      title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam cumque eum sequi voluptatibus aut corporis pariatur illum veritatis doloremque velit, cupiditate corrupti odit in vel qui. Quo, accusantium possimus.",
+      button: "View More",
+    },
+    {
+      id: 2,
+      path: img2,
+      title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam cumque eum sequi voluptatibus aut corporis pariatur illum veritatis doloremque velit, cupiditate corrupti odit in vel qui. Quo, accusantium possimus.",
+      button: "View More",
+    },
+    {
+      id: 3,
+      path: img3,
+      title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam cumque eum sequi voluptatibus aut corporis pariatur illum veritatis doloremque velit, cupiditate corrupti odit in vel qui. Quo, accusantium possimus.",
+      button: "View More",
+    },
+    {
+      id: 4,
+      path: img4,
+      title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam cumque eum sequi voluptatibus aut corporis pariatur illum veritatis doloremque velit, cupiditate corrupti odit in vel qui. Quo, accusantium possimus.",
+      button: "View More",
+    },
+  ],
+};
